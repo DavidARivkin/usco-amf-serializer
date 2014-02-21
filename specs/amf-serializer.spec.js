065747d2054905cf4aec788ff7f2ed53104916c4
Mark Moissette <kaosat.dev@gmail.com>
2014-02-21 15:35:54 +0100

    - lots of additional improvements and cleanup:  * zipped version is now actually compressed (although there seem to be issue in js-zip with reopening compressed files)  * generation of amf object entries is now correct regardless of position of object in object hierarchy  * generation of constellation is now done correctly as well  * updated tests/ cleaned up useless files

--- specs/amf-serializer.spec.js
+++ specs/amf-serializer.spec.js
@@ -14,24 +14,37 @@ describe("AMF serializer tests", function() {
     var expGeneratedAMF = fs.readFileSync("specs/data/cube.amf", "utf8")
     expect(obsGeneratedAMF).toEqual(expGeneratedAMF);
   });
-
+  
+  
   it("can serialize to compressed amf files", function() {
     var object = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
 
-    var obsGeneratedAMF = getZippedContent( serializer.serialize(object,true, "cube.amf") );
+    var obsGeneratedAMF = getZippedContent( serializer.serialize(object, true, "cube.amf") );
+    //fs.writeFileSync("specs/data/cube_zipped.amf",serializer.serialize(object, true, "cube.amf"));
     var expGeneratedAMF = getZippedContent( fs.readFileSync("specs/data/cube_zipped.amf", "binary") );
 
     expect(obsGeneratedAMF).toEqual(expGeneratedAMF);
   });
 
-  /*it("can serialize an object hierarchy to amf files", function() {
+  it("can serialize an object hierarchy to amf files", function() {
+    var root = new THREE.Object3D();
     var object = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
     var subObject = new THREE.Mesh(new THREE.SphereGeometry(10,10,10),new THREE.MeshBasicMaterial);
+    var subSubObject = new THREE.Mesh(new THREE.CubeGeometry(10,10,10),new THREE.MeshBasicMaterial);
+    
+    object.rotation.x = 1;
     subObject.position.x = 20;
-    object.add( subObject );
+    subSubObject.rotation.z = 1;
+    subSubObject.position.z = 74.2;
 
-    var obsGeneratedAMF = serializer.serialize(object);
+    root.add( object );
+    object.add( subObject );
+    subObject.add( subSubObject );
+
+
+    var obsGeneratedAMF = serializer.serialize(root);
+    //fs.writeFileSync("specs/data/hierarchy.amf",obsGeneratedAMF);
     var expGeneratedAMF = fs.readFileSync("specs/data/hierarchy.amf", "utf8")
     expect(obsGeneratedAMF).toEqual(expGeneratedAMF);
-  });*/
+  });
 });
